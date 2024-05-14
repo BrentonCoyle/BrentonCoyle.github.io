81b6160abe11d498c028873beafd9840793bb861
Brenton <brentoncoyle1@gmail.com>
2024-05-13 22:20:24 -0500

    named the pages

--- JAVASCRIPT/pokemon.js
+++ JAVASCRIPT/pokemon.js
@@ -17,31 +17,20 @@ function getPokemon() {
             const pokemonName = data.name;
             const pokemonAbilities = data.abilities.map(ability => ability.ability.name);
             const pokemonTypes = data.types.map(type => type.type.name);
-
-            console.log('Name:', pokemonName);
-            console.log('Abilities:', pokemonAbilities);
-            console.log('Types:', pokemonTypes);
-
-            // Get the URL of the default front image
             const imageUrl = data.sprites.front_default;
-            console.log('Image URL:', imageUrl);
+            
 
-            // Create HTML string for the Pokémon data
+        
             const pokeStuff = `
-            
                 <img id="pokeImage" src="${imageUrl}" alt="${pokemonName}">
-            
-
+        
                 <div id="pokeText">
                     <p>Name: ${pokemonName}</p>
                     <p>Abilities: ${pokemonAbilities.join(', ')}</p>
                     <p>Types: ${pokemonTypes.join(', ')}</p>
-                </div>
-                
-            `;
-
-            // Append the HTML string to the pokeData div
-            const pokeData = document.getElementById('pokeData');
+                </div>`;
+            
+                const pokeData = document.getElementById('pokeData');
             pokeData.innerHTML = pokeStuff;
         })
         .catch(error => {
